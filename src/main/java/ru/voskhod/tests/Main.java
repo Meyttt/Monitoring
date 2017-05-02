ef4a6c83856cbef3a10a127e9fe05ea23d9f25a2
meyttt <sabermeyt@gmail.com>
2017-05-02 14:57:55 +0300

    Подробности + общий лог

--- src/main/java/ru/voskhod/tests/Main.java
+++ src/main/java/ru/voskhod/tests/Main.java
@@ -2,6 +2,8 @@ package ru.voskhod.tests;
 
 import org.apache.log4j.Logger;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
 
@@ -9,24 +11,39 @@ import java.util.Date;
  * Created by a.chebotareva on 13.04.2017.
  */
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         Walker walker = new Walker();
         Logger logger = Logger.getLogger(Main.class);
-        logger.warn("Проверка мониторинга ГУЦ от "+new Date());
+        logger.warn("Проверка подсистемы мониторинга от "+new Date());
+        File log = new File(new File(".").getAbsolutePath()+"\\..\\"+"log.txt");
+        FileWriter fileWriter = new FileWriter(log,true);
         try{
             walker.walk();
             logger.warn("Проверка прошла успешно.");
+            fileWriter.append("Проверка подсистемы мониторинга прошла успешно\n");
         } catch (AssertionError e) {
             e.printStackTrace();
             logger.error("Ошибка на странице "+walker.currentUrl);
+            logger.error(stackTraceToString(e));
             logger.error("Проверка провалена.");
+            fileWriter.append("Проверка подсистемы мониторинга провалена\n");
         } catch (Exception e) {
             logger.error("Ошибка на странице "+walker.currentUrl);
-            logger.error(e.getMessage());
+            logger.error(stackTraceToString(e));
             logger.error("Проверка провалена.");
+            fileWriter.append("Проверка подсистемы мониторинга провалена\n");
         } finally {
+            fileWriter.flush();
             walker.closeDriver();
         }
+    }
+    public static String stackTraceToString(Throwable e){
+        StringBuilder stringBuilder = new StringBuilder();
+        for(StackTraceElement stackTraceElement:e.getStackTrace()){
+            stringBuilder.append(stackTraceElement.toString());
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
     }
 
 
--- src/main/java/ru/voskhod/tests/Walker.java
+++ src/main/java/ru/voskhod/tests/Walker.java
@@ -1,5 +1,6 @@
 package ru.voskhod.tests;
 
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -20,6 +21,7 @@ public class Walker {
     WebDriver driver;
     WebDriverWait wait;
     String currentUrl;
+    Logger logger = Logger.getLogger(Main.class);
 
     public void initDriver() throws IOException {
         System.setProperty("webdriver.chrome.driver", "data/chromedriver.exe");
@@ -33,31 +35,41 @@ public class Walker {
     public void walk() throws IOException {
         initDriver();
         driver.get(config.get("urlMain"));
-
+        
         wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("ВХОД В СИСТЕМУ")));
         driver.findElement(By.xpath("//*[@id=\"Login\"]")).sendKeys(config.get("login"));
         driver.findElement(By.xpath("//*[@id=\"Password\"]")).sendKeys(config.get("password"));
         driver.findElement(By.xpath("/html/body/div/div[3]/div/form/fieldset/table/tbody/tr[3]/td/input")).submit();
+        logger.info("Проверка доступности страницы "+ config.get("service"));
         currentUrl=config.get("service");
         driver.get(currentUrl);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/div[3]/div[1]")));
         Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/div[3]/div[1]")).getText().contains("Результаты мониторинга"));
+        logger.info("Успешно.");
+        logger.info("Проверка доступности страницы "+ config.get("serts"));
         currentUrl=config.get("serts");
         driver.get(currentUrl);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/div[3]/div[1]")));
         Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/div[3]/div[1]")).getText().contains("Информация о событиях"));
+        logger.info("Успешно.");
+        logger.info("Проверка доступности страницы "+ config.get("users"));
         currentUrl=config.get("users");
         driver.get(currentUrl);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/div[3]/div[1]")));
         Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/div[3]/div[1]")).getText().contains("Перечень пользователей подсистемы"));
+        logger.info("Успешно.");
+        logger.info("Проверка доступности страницы "+ config.get("roles"));
         currentUrl=config.get("roles");
         driver.get(currentUrl);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/div[3]/div[1]")));
         Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/div[3]/div[1]")).getText().contains("Перечень ролей пользователей подсистемы"));
+        logger.info("Успешно.");
+        logger.info("Проверка доступности страницы "+ config.get("settings"));
         currentUrl=config.get("settings");
         driver.get(currentUrl);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/div[3]/div[1]")));
         Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/div[3]/div[1]")).getText().contains("Перечень параметров подсистемы"));
+        logger.info("Успешно.");
     }
     public void closeDriver() throws InterruptedException {
         if(driver!=null){
