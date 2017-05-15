8516ba84074039c26cb351c54bf767ef27d78141
meyttt <sabermeyt@gmail.com>
2017-05-15 09:56:55 +0300

    from server1

--- src/main/java/ru/voskhod/tests/Main.java
+++ src/main/java/ru/voskhod/tests/Main.java
@@ -12,36 +12,43 @@ import java.util.Date;
  */
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
-        Walker walker = new Walker();
+        Walker walker = null;
         Logger logger = Logger.getLogger(Main.class);
-        logger.warn("Проверка подсистемы мониторинга от "+new Date());
+        logger.info("********Проверка подсистемы мониторинга от "+new Date()+"********");
         File log = new File(new File(".").getAbsolutePath()+"\\..\\"+"log.txt");
         FileWriter fileWriter = new FileWriter(log,true);
         try{
+            walker = new Walker();
             walker.walk();
-            logger.warn("Проверка прошла успешно.");
-            fileWriter.append("Проверка подсистемы мониторинга прошла успешно\n");
+            logger.info("Проверка прошла успешно.");
+            fileWriter.append("Проверка подсистемы мониторинга прошла успешно\r\n");
+            fileWriter.flush();
+            walker.closeDriver();
+            System.exit(0);
         } catch (AssertionError e) {
             e.printStackTrace();
             logger.error("Ошибка на странице "+walker.currentUrl);
             logger.error(stackTraceToString(e));
             logger.error("Проверка провалена.");
-            fileWriter.append("Проверка подсистемы мониторинга провалена\n");
+            fileWriter.append("Проверка подсистемы мониторинга провалена\r\n");
+            fileWriter.flush();
+            walker.closeDriver();
+            System.exit(1);
         } catch (Exception e) {
             logger.error("Ошибка на странице "+walker.currentUrl);
             logger.error(stackTraceToString(e));
             logger.error("Проверка провалена.");
-            fileWriter.append("Проверка подсистемы мониторинга провалена\n");
-        } finally {
+            fileWriter.append("Проверка подсистемы мониторинга провалена\r\n");
             fileWriter.flush();
             walker.closeDriver();
+            System.exit(1);
         }
     }
     public static String stackTraceToString(Throwable e){
         StringBuilder stringBuilder = new StringBuilder();
         for(StackTraceElement stackTraceElement:e.getStackTrace()){
             stringBuilder.append(stackTraceElement.toString());
-            stringBuilder.append("\n");
+            stringBuilder.append("\r\n");
         }
         return stringBuilder.toString();
     }
